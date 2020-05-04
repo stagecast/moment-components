4048867039e1f44232af893984e677d0eaf7e1d9
Filippo Boiani <filippo.boiani2@gmail.com>
2020-05-04 16:01:10 +0200

    feature(locale): fixed italian translation file

--- src/locale/lang/it.js
+++ src/locale/lang/it.js
@@ -1,17 +1,19 @@
 export default {
-  help: {
-    tabs: {
-      prize: 'Premi',
-      howto: 'Istruzioni',
-      share: 'Condividi',
+  scComponents: { 
+    help: {
+      tabs: {
+        prize: 'Premi',
+        howto: 'Istruzioni',
+        share: 'Condividi',
+      },
+      start: 'Cominciamo!',
+      share: {
+        title: 'Copia il link qui sotto ed invialo ai tuoi amici. Il link gli permetterà di partecipare al Momento dal Browser.'
+      }
     },
-    start: 'Cominciamo!',
-    share: {
-      title: 'Copia il link qui sotto ed invialo ai tuoi amici. Il link gli permetterà di partecipare al Momento dal Browser.'
+    prize: {
+      congrats: 'Congratulazioni! Hai vinto un premio.',
+      claim: 'Vai al Premio'
     }
-  },
-  prize: {
-    congrats: 'Congratulazioni! Hai vinto un premio.',
-    claim: 'Vai al Premio'
   }
 }
